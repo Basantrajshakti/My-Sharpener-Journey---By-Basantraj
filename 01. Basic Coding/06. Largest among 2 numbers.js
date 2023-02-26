2141ad0f455e70b5086925214f8d08aa27a1b82b
Basantrajshakti <basantrajshakti@gmail.com>
2023-02-26 21:39:58 +0530

    WAP. Largest among 2 numbers

---
+++ 01. Basic Coding/06. Largest among 2 numbers.js
@@ -0,0 +1,8 @@
+function max(a, b) {
+  /*write the code to find the maximum between two numbers below 
+   only print the maximum number  */
+  if (a > b)
+    console.log(a);
+  else
+    console.log(b);
+}
